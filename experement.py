b10924fe4211a1aba687225f0cd04da676441df6
stass <stas.sor@gmail.com>
2021-12-04 15:35:58 +0300

    отталкиваемся от этого

---
+++ experement.py
@@ -0,0 +1,18 @@
+import torch
+import pandas as pd
+import yaml
+import torchvision
+
+# Model
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+
+# Images
+path = input("foto: ")
+img = path  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+results = model(img)
+
+# Results
+results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
